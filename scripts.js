acbb91c6b2f5b442aa72ef8d98753716fd9c5bc9
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-03-02 23:31:43 -0300

    02/03/2025

--- scripts.js
+++ scripts.js
@@ -1,19 +1,39 @@
-//Arraste das seções
 window.onload = function(){
 //Troca de icones
 let icones = [...document.querySelectorAll("#for_sections div img")];
 let srcs= [...(icones.map(el => el.src))]
 
-//Rolagem das seções
+//limites de rolagem para anuncios
+const imgs_anun = document.getElementById('imgs_anun');
+let larg_anuncio = document.getElementById('anuncio').offsetWidth;
+let gap_imgs_anuncio = parseFloat(getComputedStyle(document.getElementById('imgs_anun')).getPropertyValue('gap'));
+let quant_anun = [...document.querySelectorAll('#imgs_anun>img')].length;
+let larg_img = document.querySelector('#imgs_anun>img').offsetWidth;
+let larg_imgs_anun = gap_imgs_anuncio *(quant_anun-1) + larg_img * quant_anun;
+let inicio = 0
+let meio = larg_anuncio/2 - larg_imgs_anun/2;
+let fim = larg_anuncio - larg_imgs_anun;
+
+imgs_anun.style.transform = `translateX(${meio}px)`; 
+
+
+//limites de rolagem para seções
 let part_sections = document.getElementById('part_sections');
 let secoes = document.getElementById('for_sections');
-
 const style_sections = getComputedStyle(secoes);
 let gap = parseFloat(style_sections.gap);
 let width_sections = 1200 + (gap * 12);
-
 let larg_partSections = part_sections.offsetWidth;
 let limite = -(width_sections - larg_partSections);
+
+//limites de rolagem para produtos
+let larg_for_promos = document.getElementById('for_promos').offsetWidth;
+let gap_parte = parseFloat(getComputedStyle(document.getElementById('parteUm')).getPropertyValue('gap'));
+let quant_part = [...document.querySelectorAll('#parteUm > div')].length;
+let larg_prod = document.querySelector('.for_prod').offsetWidth;
+let larg_part = quant_part * larg_prod + gap_parte*(quant_part-1); 
+let limitePromos = larg_for_promos - larg_part; 
+console.log(larg_for_promos,quant_part,larg_prod, larg_part, limitePromos);
 
 let toc_ini = 0;
 let arraste = 0;
@@ -91,7 +111,6 @@ const finalizarArraste = () => {
   desacelerar();
 };
 
-
 secoes.addEventListener('mousedown', iniciarArraste);
 secoes.addEventListener('mousemove', aoMover);
 secoes.addEventListener('mouseup', finalizarArraste);
@@ -101,6 +120,7 @@ secoes.addEventListener('touchstart', iniciarArraste);
 secoes.addEventListener('touchmove', aoMover);
 secoes.addEventListener('touchend', finalizarArraste);
 
+//rolagem dos anuncios
 
 
 };
