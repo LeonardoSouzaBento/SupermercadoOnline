91b6943ee997cd94680e083dfa909b5bef2ab32d
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-03-03 19:11:59 -0300

    03/03/2025

---
+++ scripts2.js
@@ -0,0 +1,16 @@
+const operar=(a, b)=>{ console.log(a* b)}
+
+const aplicar2=()=>{
+    let num= 7
+    let num2 =5
+    operar(num, num2)
+}
+
+const aplicar=()=>{
+    let num= 1
+    let num2 =3
+    operar(num, num2)
+}
+
+aplicar2();
+aplicar();
