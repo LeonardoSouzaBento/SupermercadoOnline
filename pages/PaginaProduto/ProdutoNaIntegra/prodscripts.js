3d347de3cf549bb1b97e7625a4f4e1ba3badf930
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-04-12 12:15:02 -0300

    12/04/2025

--- pages/PaginaProduto/ProdutoNaIntegra/prodscripts.js
+++ pages/PaginaProduto/ProdutoNaIntegra/prodscripts.js
@@ -1,6 +1,6 @@
-window.onload = function(){
-    const mais_quant = document.querySelector('.mais_quant');
-    const div_filha = document.querySelector('.mais_quant>div');
-    let ajuste = -(mais_quant.clientWidth - div_filha.scrollWidth);
-    div_filha.style.transform = `translateX(${ajuste}px)`;
-}
+// window.onload = function(){
+//     const mais_quant = document.querySelector('.mais_quant');
+//     const div_filha = document.querySelector('.mais_quant>div');
+//     let ajuste = -(mais_quant.clientWidth - div_filha.scrollWidth);
+//     div_filha.style.transform = `translateX(${ajuste}px)`;
+// }
